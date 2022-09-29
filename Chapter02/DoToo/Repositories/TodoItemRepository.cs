3220afb1d06fff72a20ad1fb73a4fb7287d6cebc
Michael Cummings <cummings.michael@live.com>
2022-09-29 09:57:30 -0400

    Create Database Connection

--- Chapter02/DoToo/Repositories/TodoItemRepository.cs
+++ Chapter02/DoToo/Repositories/TodoItemRepository.cs
@@ -1,9 +1,11 @@
 ﻿namespace DoToo.Repositories;
 
 using DoToo.Models;
+using SQLite;
 
 public class TodoItemRepository : ITodoItemRepository
 {
+    private SQLiteAsyncConnection connection;
     public event EventHandler<TodoItem> OnItemAdded; 
     public event EventHandler<TodoItem> OnItemUpdated;
 
@@ -32,4 +34,26 @@ public class TodoItemRepository : ITodoItemRepository
         }
     }
 
+    private async Task CreateConnection()
+    {
+        if (connection != null)
+        {
+            return;
+        }
+
+        var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        var databasePath = Path.Combine(documentPath, "TodoItems.db");
+
+        connection = new SQLiteAsyncConnection(databasePath); 
+        await connection.CreateTableAsync<TodoItem>();
+
+        if (await connection.Table<TodoItem>().CountAsync() == 0)
+        {
+            await connection.InsertAsync(new TodoItem()
+            {
+                Title = "Welcome to DoToo",
+                Due = DateTime.Now
+            });
+        }
+    }
 }
