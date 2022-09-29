9fd3d9042022b5ef193e4420fea07d88e129065b
Michael Cummings <cummings.michael@live.com>
2022-09-29 09:26:44 -0400

    Define TodoItem

---
+++ Chapter02/DoToo/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace DoToo.Models;
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+
+    public DateTime Due { get; set; }
+}
