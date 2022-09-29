73a58743f8dbe3f936e8412be3fe9d900a449639
Michael Cummings <cummings.michael@live.com>
2022-09-29 09:44:13 -0400

    Update TodoItem Class

--- Chapter02/DoToo/Models/TodoItem.cs
+++ Chapter02/DoToo/Models/TodoItem.cs
@@ -1,6 +1,10 @@
 ﻿namespace DoToo.Models;
+
+using SQLite;
+
 public class TodoItem
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
     public string Title { get; set; }
     public bool Completed { get; set; }
