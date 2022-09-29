f0d64b8de89b1ee3325a63cc57b1281c3ddaf91f
Michael Cummings <cummings.michael@live.com>
2022-09-29 09:33:18 -0400

    Create Repository

---
+++ Chapter02/DoToo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DoToo.Repositories;
+
+using DoToo.Models;
+
+public interface ITodoItemRepository
+{
+    event EventHandler<TodoItem> OnItemAdded; event EventHandler<TodoItem> OnItemUpdated;
+
+    Task<List<TodoItem>> GetItems(); Task AddItem(TodoItem item); Task UpdateItem(TodoItem item); Task AddOrUpdate(TodoItem item);
+}
---
+++ Chapter02/DoToo/Repositories/TodoItemRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace DoToo.Repositories;
+
+using DoToo.Models;
+
+public class TodoItemRepository : ITodoItemRepository
+{
+    public event EventHandler<TodoItem> OnItemAdded; 
+    public event EventHandler<TodoItem> OnItemUpdated;
+
+    public async Task<List<TodoItem>> GetItems()
+    {
+        return null; // Just to make it build
+    }
+
+    public async Task AddItem(TodoItem item)
+    {
+    }
+
+    public async Task UpdateItem(TodoItem item)
+    {
+    }
+
+    public async Task AddOrUpdate(TodoItem item)
+    {
+        if (item.Id == 0)
+        {
+            await AddItem(item);
+        }
+        else
+        {
+            await UpdateItem(item);
+        }
+    }
+
+}
