24cbae948c35b8c0590047fe030297b4b50d18de
Michael Cummings <cummings.michael@live.com>
2022-09-29 10:07:27 -0400

    Define ViewModel base class

---
+++ Chapter02/DoToo/ViewModels/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace DoToo.ViewModels;
+
+using System.ComponentModel;
+
+public abstract class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void RaisePropertyChanged(params string[] propertyNames)
+    {
+        foreach (var propertyName in propertyNames)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public INavigation Navigation { get; set; }
+}
