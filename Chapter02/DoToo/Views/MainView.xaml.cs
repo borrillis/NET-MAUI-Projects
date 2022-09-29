b8e2e7ccca34e91d82b6d12ab852afe66e70ca0f
Michael Cummings <cummings.michael@live.com>
2022-09-29 11:07:02 -0400

    Create MainView view

---
+++ Chapter02/DoToo/Views/MainView.xaml.cs
@@ -0,0 +1,13 @@
+namespace DoToo.Views;
+
+using DoToo.ViewModels;
+
+public partial class MainView : ContentPage
+{
+    public MainView(MainViewModel viewModel)
+    {
+        InitializeComponent(); 
+        viewModel.Navigation = Navigation; 
+        BindingContext = viewModel;
+    }
+}
