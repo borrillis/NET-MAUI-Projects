c9e35c0dcb237d1360754ce37de48d0abc486b3e
Michael Cummings <cummings.michael@live.com>
2022-09-29 10:49:07 -0400

    Create ItemViewModel

---
+++ Chapter02/DoToo/ViewModels/ItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DoToo.ViewModels;
+
+using DoToo.Repositories;
+
+public class ItemViewModel : ViewModel
+{
+    private readonly ITodoItemRepository repository;
+
+    public ItemViewModel(ITodoItemRepository repository)
+    {
+        this.repository = repository;
+    }
+}
