ae8fad37b7dce9c9218d8952802ccf72bf48fb99
Michael Cummings <cummings.michael@live.com>
2022-09-29 09:20:01 -0400

    File-New-DotNet-MAUI

---
+++ Chapter02/DoToo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DoToo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Chapter02/DoToo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DoToo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
