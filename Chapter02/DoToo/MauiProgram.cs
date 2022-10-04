8f2b82eb826f8725fae2762d9ffa3504f5d38a89
Michael Cummings <cummings.michael@live.com>
2022-10-04 10:49:03 -0400

    Add Dependecy Injection

--- Chapter02/DoToo/MauiProgram.cs
+++ Chapter02/DoToo/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿namespace DoToo;
 
+using DoToo.Repositories;
+
 public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
@@ -11,8 +13,33 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .RegisterServices()
+            .RegisterViewModels()
+            .RegisterViews();
 
 		return builder.Build();
 	}
+    public static MauiAppBuilder RegisterServices(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddSingleton<ITodoItemRepository,TodoItemRepository>();
+
+        return mauiAppBuilder;
+    }
+
+    public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddTransient<ViewModels.MainViewModel>();
+
+        mauiAppBuilder.Services.AddTransient<ViewModels.ItemViewModel>();
+        return mauiAppBuilder;
+    }
+
+    public static MauiAppBuilder RegisterViews(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddTransient<Views.MainView>();
+
+        mauiAppBuilder.Services.AddTransient<Views.ItemView>();
+        return mauiAppBuilder;
+    }
 }
