ee999192cef1544bb21d52c68d597be3036cc7e1
Michael Cummings <cummings.michael@live.com>
2022-12-28 13:05:55 -0500

    Extending the HeadlinesViewModel class

--- Chapter04/News/ViewModels/HeadlinesViewModel.cs
+++ Chapter04/News/ViewModels/HeadlinesViewModel.cs
@@ -1,8 +1,43 @@
 ﻿namespace News.ViewModels;
 
-public class HeadlinesViewModel : ViewModel
-{    
-    public HeadlinesViewModel()
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Web;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using News.Models;
+using News.Services;
+
+public partial class HeadlinesViewModel : ViewModel
+{
+    private readonly INewsService newsService;
+
+    [ObservableProperty]
+    private NewsResult currentNews;
+
+    public HeadlinesViewModel(INewsService newsService)
     {
+        this.newsService = newsService;
+    }
+
+    public async Task Initialize(string scope) => await Initialize(scope.ToLower() switch
+    {
+        "local" => NewsScope.Local,
+        "global" => NewsScope.Global,
+        "headlines" => NewsScope.Headlines,
+        _ => NewsScope.Headlines
+    });
+
+    public async Task Initialize(NewsScope scope)
+    {
+        CurrentNews = await newsService.GetNews(scope);
+    }
+
+    [RelayCommand]
+    public void ItemSelected(object selectedItem)
+    {
+        var selectedArticle = selectedItem as Article;
+        var url = HttpUtility.UrlEncode(selectedArticle.Url);
+        // Placeholder for more code later on
     }
 }
