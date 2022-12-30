8b7ae3b70aa997e682b1c39126807ca342f404a6
Michael Cummings <cummings.michael@live.com>
2022-12-30 09:08:22 -0500

    Extending the ArticleView to receive query data

--- Chapter04/News/Views/ArticleView.xaml.cs
+++ Chapter04/News/Views/ArticleView.xaml.cs
@@ -1,8 +1,22 @@
 namespace News.Views;
 
+using System.Web;
+
+[QueryProperty("Url", "url")]
 public partial class ArticleView : ContentPage
 {
-	public ArticleView()
+    public string Url
+    {
+        set
+        {
+            BindingContext = new UrlWebViewSource
+            {
+                Url = HttpUtility.UrlDecode(value)
+            };
+        }
+    }
+
+    public ArticleView()
 	{
 		InitializeComponent();
 	}
