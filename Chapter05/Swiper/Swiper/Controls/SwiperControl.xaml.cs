d031be32526c9ecaab9010720adf77a60c439916
Michael Cummings <cummings.michael@live.com>
2022-12-30 16:25:13 -0500

    Setting the source

--- Chapter05/Swiper/Swiper/Controls/SwiperControl.xaml.cs
+++ Chapter05/Swiper/Swiper/Controls/SwiperControl.xaml.cs
@@ -1,9 +1,14 @@
 namespace Swiper.Controls;
 
+using Swiper.Utils;
+
 public partial class SwiperControl : ContentView
 {
-	public SwiperControl()
-	{
-		InitializeComponent();
-	}
+    public SwiperControl()
+    {
+        InitializeComponent();
+        var picture = new Picture();
+        descriptionLabel.Text = picture.Description;
+        image.Source = new UriImageSource() { Uri = picture.Uri };
+    }
 }
