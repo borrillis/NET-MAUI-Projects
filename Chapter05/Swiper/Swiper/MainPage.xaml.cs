dfbd7d0fca3d12108437b05104c1b0962ab69825
Michael Cummings <cummings.michael@live.com>
2022-12-30 17:06:49 -0500

    Making the call from the constructor

--- Chapter05/Swiper/Swiper/MainPage.xaml.cs
+++ Chapter05/Swiper/Swiper/MainPage.xaml.cs
@@ -7,7 +7,7 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-        MainGrid.Children.Add(new SwiperControl());
+        AddInitialPhotos();
     }
 
     private void AddInitialPhotos()
