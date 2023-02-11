8fe1238f881b23af42abb23e783e7d53426836c7
Michael Cummings <cummings.michael@live.com>
2023-02-11 01:19:39 -0500

    Get and display the current location

--- Chapter07/MeTracker/Views/MainView.xaml.cs
+++ Chapter07/MeTracker/Views/MainView.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Maps;
+
 namespace MeTracker.Views;
 
 public partial class MainView : ContentPage
@@ -5,5 +7,23 @@ public partial class MainView : ContentPage
 	public MainView()
 	{
 		InitializeComponent();
-	}
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            var status = await AppPermissions.CheckAndRequestRequiredPermissionAsync();
+            if (status == PermissionStatus.Granted)
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location == null)
+                {
+                    location = await Geolocation.GetLocationAsync();
+                }
+
+                Map.MoveToRegion(MapSpan.FromCenterAndRadius(
+                    location,
+                    Distance.FromKilometers(5)));
+            }
+        });
+
+    }
 }
