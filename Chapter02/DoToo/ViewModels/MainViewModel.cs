BionStt/MAUI-Projects-3rd-Edition

Navigating from MainView to ItemView

Chapter02/DoToo/ViewModels/MainViewModel.cs
@@ -1,19 +1,25 @@
 ﻿namespace DoToo.ViewModels;
 
+using CommunityToolkit.Mvvm.Input;
 using DoToo.Repositories;
+using DoToo.Views;
 
-public class MainViewModel : ViewModel
+public partial class MainViewModel : ViewModel
 {
     private readonly ITodoItemRepository repository;
+    private readonly IServiceProvider services;
 
-    public MainViewModel(ITodoItemRepository repository)
+    public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
     {
-        this.repository = repository; 
+        this.repository = repository;
+        this.services = services;
         Task.Run(async () => await LoadData());
     }
-
     private async Task LoadData()
     {
-
     }
+
+    [RelayCommand]
+    public async Task AddItem() => await Navigation.PushAsync(services.GetRequiredService<ItemView>());
+
 }

borrillis/NET-MAUI-Projects

Chapter02/DoToo/ViewModels/MainViewModel.cs
@@ -1,19 +1,26 @@
 ﻿namespace DoToo.ViewModels;
 
+using CommunityToolkit.Mvvm.Input;
 using DoToo.Repositories;
+using DoToo.Views;
 
-public class MainViewModel : ViewModel
+public partial class MainViewModel : ViewModel
 {
     private readonly ITodoItemRepository repository;
+    private readonly IServiceProvider services;
 
-    public MainViewModel(ITodoItemRepository repository)
+    public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
     {
-        this.repository = repository; 
+        this.repository = repository;
+        this.services = services;
         Task.Run(async () => await LoadDataAsync());
     }
 
     private async Task LoadDataAsync()
     {
-
     }
+
+    [RelayCommand]
+    public async Task AddItemAsync() => await Navigation.PushAsync(services.GetRequiredService<ItemView>());
+
 }