BionStt/MAUI-Projects-3rd-Edition

Implement [Get,Add,Update]Items() methods

Chapter02/DoToo/Repositories/TodoItemRepository.cs
@@ -11,15 +11,22 @@ public class TodoItemRepository : ITodoItemRepository
 
     public async Task<List<TodoItem>> GetItems()
     {
-        return null; // Just to make it build
+        await CreateConnection();
+        return await connection.Table<TodoItem>().ToListAsync();
     }
 
     public async Task AddItem(TodoItem item)
     {
+        await CreateConnection();
+        await connection.InsertAsync(item); 
+        OnItemAdded?.Invoke(this, item);
     }
 
     public async Task UpdateItem(TodoItem item)
     {
+        await CreateConnection();
+        await connection.UpdateAsync(item); 
+        OnItemUpdated?.Invoke(this, item);
     }
 
     public async Task AddOrUpdate(TodoItem item)

borrillis/NET-MAUI-Projects

Chapter02/DoToo/Repositories/TodoItemRepository.cs
@@ -11,15 +11,22 @@ public class TodoItemRepository : ITodoItemRepository
 
     public async Task<List<TodoItem>> GetItemsAsync()
     {
-        return null; // Just to make it build
+        await CreateConnectionAsync();
+        return await connection.Table<TodoItem>().ToListAsync();
     }
 
     public async Task AddItemAsync(TodoItem item)
     {
+        await CreateConnectionAsync();
+        await connection.InsertAsync(item); 
+        OnItemAdded?.Invoke(this, item);
     }
 
     public async Task UpdateItemAsync(TodoItem item)
     {
+        await CreateConnectionAsync();
+        await connection.UpdateAsync(item); 
+        OnItemUpdated?.Invoke(this, item);
     }
 
     public async Task AddOrUpdateAsync(TodoItem item)