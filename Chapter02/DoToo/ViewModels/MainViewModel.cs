BionStt/MAUI-Projects-3rd-Edition

Create MainViewModel

Chapter02/DoToo/ViewModels/MainViewModel.cs
@@ -1,0 +1,19 @@
+﻿namespace DoToo.ViewModels;
+
+using DoToo.Repositories;
+
+public class MainViewModel : ViewModel
+{
+    private readonly ITodoItemRepository repository;
+
+    public MainViewModel(ITodoItemRepository repository)
+    {
+        this.repository = repository; 
+        Task.Run(async () => await LoadData());
+    }
+
+    private async Task LoadData()
+    {
+
+    }
+}

borrillis/NET-MAUI-Projects

Chapter02/DoToo/ViewModels/MainViewModel.cs
@@ -1,0 +1,19 @@
+﻿namespace DoToo.ViewModels;
+
+using DoToo.Repositories;
+
+public class MainViewModel : ViewModel
+{
+    private readonly ITodoItemRepository repository;
+
+    public MainViewModel(ITodoItemRepository repository)
+    {
+        this.repository = repository; 
+        Task.Run(async () => await LoadDataAsync());
+    }
+
+    private async Task LoadDataAsync()
+    {
+
+    }
+}
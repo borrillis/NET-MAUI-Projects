c5a08d8528f2db325b88da458292e0b894fd1a26
Michael Cummings <cummings.michael@live.com>
2022-09-29 10:44:27 -0400

    Create MainViewModel

---
+++ Chapter02/DoToo/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DoToo.ViewModels;
+
+using DoToo.Repositories;
+
+public class MainViewModel : ViewModel
+{
+    private readonly ITodoItemRepository repository;
+
+    public MainViewModel(ITodoItemRepository repository)
+    {
+        this.repository = repository; 
+        Task.Run(async () => await LoadDataAsync());
+    }
+
+    private async Task LoadDataAsync()
+    {
+
+    }
+}
