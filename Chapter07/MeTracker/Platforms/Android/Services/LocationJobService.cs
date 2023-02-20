efa74c3810f34f7813eb8ff06c974a84faf86e73
Michael Cummings <cummings.michael@live.com>
2023-02-20 18:15:16 -0500

    Creating a background job

---
+++ Chapter07/MeTracker/Platforms/Android/Services/LocationJobService.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.App.Job;
+
+namespace MeTracker.Platforms.Android.Services;
+
+[Service(Name = "MeTracker.Platforms.Android.Services.LocationJobService", Permission = "android.permission.BIND_JOB_SERVICE")]
+internal class LocationJobService : JobService
+{ 
+    public override bool OnStartJob(JobParameters @params)
+    {
+        return true;
+    }
+
+    public override bool OnStopJob(JobParameters @params)
+    {
+        return true;
+    }
+}
+
