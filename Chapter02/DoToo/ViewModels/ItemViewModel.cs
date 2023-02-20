BionStt/MAUI-Projects-3rd-Edition

Adding new items to the list

Chapter02/DoToo/ViewModels/ItemViewModel.cs
@@ -1,13 +1,28 @@
 ﻿namespace DoToo.ViewModels;
 
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DoToo.Models;
 using DoToo.Repositories;
 
-public class ItemViewModel : ViewModel
+public partial class ItemViewModel : ViewModel
 {
     private readonly ITodoItemRepository repository;
+
+    [ObservableProperty]
+    TodoItem item;
 
     public ItemViewModel(ITodoItemRepository repository)
     {
         this.repository = repository;
+        Item = new TodoItem() { Due = DateTime.Now.AddDays(1) };
     }
+
+    [RelayCommand]
+    public async Task Save()
+    {
+        await repository.AddOrUpdate(Item);
+        await Navigation.PopAsync();
+    }
+
 }

borrillis/NET-MAUI-Projects

Chapter02/DoToo/ViewModels/ItemViewModel.cs
@@ -1,13 +1,28 @@
 ﻿namespace DoToo.ViewModels;
 
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DoToo.Models;
 using DoToo.Repositories;
 
-public class ItemViewModel : ViewModel
+public partial class ItemViewModel : ViewModel
 {
     private readonly ITodoItemRepository repository;
+
+    [ObservableProperty]
+    TodoItem item;
 
     public ItemViewModel(ITodoItemRepository repository)
     {
         this.repository = repository;
+        Item = new TodoItem() { Due = DateTime.Now.AddDays(1) };
     }
+
+    [RelayCommand]
+    public async Task SaveAsync()
+    {
+        await repository.AddOrUpdateAsync(Item);
+        await Navigation.PopAsync();
+    }
+
 }