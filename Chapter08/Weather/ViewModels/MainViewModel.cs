bda233ae911e357b1cb6c29c02cc4e4a35f1b203
Michael Cummings <cummings.michael@live.com>
2023-03-11 09:15:16 -0500

    Creating the ViewModel class

---
+++ Chapter08/Weather/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Weather.Services;
+
+namespace Weather.ViewModels;
+
+public partial class MainViewModel : ViewModel
+{
+    private readonly IWeatherService weatherService;
+
+    public MainViewModel(IWeatherService weatherService)
+    {
+        this.weatherService = weatherService;
+    }
+}
