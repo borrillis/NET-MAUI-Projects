a70f94dd980f6f54bbc6f303c5bd37967871a4da
Michael Cummings <cummings.michael@live.com>
2023-07-10 09:47:25 -0400

    Initialize the ML.Net model

--- Chapter12/HotdogOrNot/ImageClassifier/MLNetClassifier.cs
+++ Chapter12/HotdogOrNot/ImageClassifier/MLNetClassifier.cs
@@ -1,9 +1,21 @@
+﻿using Microsoft.ML.OnnxRuntime;
 namespace HotdogOrNot.ImageClassifier;
 
 internal class MLNetClassifier : IClassifier
 {
+    readonly InferenceSession session;
+    readonly bool isBgr;
+    readonly bool isRange255;
+    readonly string inputName;
+    readonly int inputSize;
+
     public MLNetClassifier(byte[] model) 
     {
+        session = new InferenceSession(model);
+        isBgr = session.ModelMetadata.CustomMetadataMap["Image.BitmapPixelFormat"] == "Bgr8";
+        isRange255 = session.ModelMetadata.CustomMetadataMap["Image.NominalPixelRange"] == "NominalRange_0_255";
+        inputName = session.InputMetadata.Keys.First();
+        inputSize = session.InputMetadata[inputName].Dimensions[2];
     }
 
     public ClassifierOutput Classify(byte[] imageBytes)
