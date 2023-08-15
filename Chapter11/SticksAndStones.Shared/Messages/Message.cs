399cfa7c8ed372c19887edc7e2d4e3b74d075230
Michael Cummings <cummings.michael@live.com>
2023-08-15 15:56:31 -0400

    Creating the Connect Models

---
+++ Chapter11/SticksAndStones.Shared/Messages/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SticksAndStones.Messages;
+
+public abstract class Message
+{
+    public Type TypeInfo { get { return this.GetType(); } }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+    public Guid SenderId { get; set; }
+
+    public Message (Guid senderId) { this.SenderId = senderId; }
+}
---
+++ Chapter11/SticksAndStones.Shared/Models/Game.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace SticksAndStones.Models;
+
+public class Game
+{
+    public Guid Id { get; set; } = Guid.Empty;
+
+    public Player PlayerOne { get; set; }
+    public Player PlayerTwo { get; set; }
+
+    public Player NextPlayer { get; set; }
+
+    public List<int> Sticks = new List<int>(24);
+    public List<int> Stones = new List<int>(9);
+    public List<int> Score = new List<int>(2);
+
+    public bool Completed { get; set; } = false;
+
+    public int PlayerOneScore
+    {
+        get => Score[0];
+        set => Score[0] = value;
+    }
+
+    public int PlayerTwoScore
+    {
+        get => Score[1];
+        set => Score[1] = value;
+    }
+
+    public static Game New(Player challenger, Player opponent)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            PlayerOne = opponent,
+            PlayerTwo = challenger,
+            NextPlayer = opponent
+        };
+    }
+}
+
+
