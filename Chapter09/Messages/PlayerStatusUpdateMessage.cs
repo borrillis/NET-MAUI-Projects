97a858cb9168eec971e239111910b209d2cf3bac
Michael Cummings <cummings.michael@live.com>
2023-08-15 15:56:31 -0400

    Creating the Connect Models

---
+++ Chapter09/Messages/PlayerStatusUpdateMessage.cs
@@ -0,0 +1,14 @@
+﻿using SticksAndStones.Models;
+
+namespace SticksAndStones.Messages
+{
+    public class PlayerStatusUpdateMessage : Message
+    {
+        private readonly Player Player;
+
+        public PlayerStatusUpdateMessage(Player player) : base(player.Id)
+        {
+            this.Player = player;
+        }
+    }
+}
---
+++ Chapter09/SticksAndStones.Functions/Repository/GameDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SticksAndStones.Models;
+
+namespace SticksAndStones.Repository;
+
+internal class GameDbContext : DbContext
+{
+    public GameDbContext(DbContextOptions<GameDbContext> options) : base(options) { }
+
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Game> Games { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Player>()
+            .HasKey(p => p.Id);
+
+        modelBuilder.Entity<Game>()
+            .HasKey(g => g.Id);
+        modelBuilder.Entity<Game>()
+            .HasOne(g => g.PlayerOne).WithOne(p => p.Game);
+        modelBuilder.Entity<Game>()
+            .HasOne(e => e.PlayerTwo).WithOne(p => p.Game);
+        modelBuilder.Entity<Game>()
+            .HasOne(e => e.NextPlayer).WithOne(p => p.Game);
+    }
+}
