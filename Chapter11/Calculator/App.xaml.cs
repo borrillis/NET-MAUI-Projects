c78ac758257f855ad49a5b8d54cea3c6f628d332
Michael Cummings <cummings.michael@live.com>
2023-11-19 00:43:08 -0500

    resizing the main window

--- Chapter11/Calculator/App.xaml.cs
+++ Chapter11/Calculator/App.xaml.cs
@@ -12,5 +12,35 @@
             base.OnHandlerChanging(args);
             MainPage = args.NewHandler.MauiContext.Services.GetService<MainPage>();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            if (OperatingSystem.IsWindows() || OperatingSystem.IsMacCatalyst())
+            {
+                window.Created += Window_Created;
+            }
+            return window;
+        }
+
+        private async void Window_Created(object sender, EventArgs e)
+        {
+            const int defaultWidth = 450;
+            const int defaultHeight = 800;
+
+            var window = (Window)sender;
+            window.Width = defaultWidth;
+            window.Height = defaultHeight;
+            window.X = -defaultWidth;
+            window.Y = -defaultHeight;
+
+            await window.Dispatcher.DispatchAsync(() => { });
+
+            var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+            window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2;
+            window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
+
+            window.Created -= Window_Created;
+        }
     }
 }
