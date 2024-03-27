9ff2dbeeb56a9b6bd1616eccfccc84fbbf78a6f5
Michael Cummings <cummings.michael@live.com>
2024-03-27 09:34:04 -0400

    Update Chapter03 to net8

--- Chapter03/Chat/BuildChat/BuildChat.Android/MainActivity.cs
+++ Chapter03/Chat/BuildChat/BuildChat.Android/MainActivity.cs
@@ -1,21 +1,12 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
 using Microsoft.Maui;
 
 namespace BuildChat.Droid
 {
-    [Activity(Label = "BuildChat", Icon = "@mipmap/icon", Theme = "@style/Theme.MaterialComponents", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+
     public class MainActivity : MauiAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-        }
     }
 }
